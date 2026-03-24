40acfb78d313bc377edd17757ff645a3c70c1e68
ERR0RW0LF <finn.woma@gmail.com>
2026-03-24 17:35:03 +0100

    fix: update log file path in init script and modify start script to capture output of p2p_pager

--- library/user/pager2pager/p2p_pager/start_p2p_pager.sh
+++ library/user/pager2pager/p2p_pager/start_p2p_pager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Service started" > /tmp/p2p_pager.log
-exec /usr/bin/python3 /usr/bin/p2p_pager >> /tmp/p2p_pager.log 2>&1
+echo "$(/usr/bin/python3 /usr/bin/p2p_pager 2>&1)" >> /tmp/p2p_pager.log
