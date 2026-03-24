8119204e1432733448ab727fa0a03e67daedfca3
ERR0RW0LF <finn.woma@gmail.com>
2026-03-24 11:58:34 +0100

    fix: improve service stop function and use exec in start script for better process management

--- library/user/pager2pager/p2p_pager/start_p2p_pager.sh
+++ library/user/pager2pager/p2p_pager/start_p2p_pager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Service started" > /tmp/p2p_pager.log
-/usr/bin/python3 /usr/bin/p2p_pager >> /tmp/p2p_pager.log 2>&1
+exec /usr/bin/python3 /usr/bin/p2p_pager >> /tmp/p2p_pager.log 2>&1
