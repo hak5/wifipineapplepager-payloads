89fa6a07f5a87cb214f79b22db01524f2e8637d5
f3bandit <f3bandit@gmail.com>
2026-04-09 23:50:19 -0400

    Update magic.ps1

--- library/user/exfiltration/wifi_loot_viewer/magic.ps1
+++ library/user/exfiltration/wifi_loot_viewer/magic.ps1
@@ -21,7 +21,8 @@ $RemoteDir = "/mmc/root/loot/wifi/"
 $TempKey   = Join-Path $env:TEMP ("pager_key_" + [guid]::NewGuid().ToString())
 
 # ---- PASTE BASE64 OF YOUR WORKING PRIVATE KEY FILE BELOW ----
-$PrivateKeyB64 = @'INSERT AUTHORIZED HERE
+$PrivateKeyB64 = @'
+AUTHORIZED KEY HERE
 '@
 
 function Test-CommandExists {
@@ -81,15 +82,3 @@ finally {
         Remove-Item $TempKey -Force -ErrorAction SilentlyContinue
     }
 }
-
-# empty temp folder
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-
-# delete run box history
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-# Delete powershell history
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-# Empty recycle bin
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
